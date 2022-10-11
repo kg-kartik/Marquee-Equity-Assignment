f4bae7395536a979193c90c2e74fa31a2bb22c7b
kg-kartik <goel.kartik39@gmail.com>
2022-10-11 13:05:20 +0530

    cra setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchInput from "./components/SearchInput";
+
+const App = () => {
+  return (
+    <>
+		  <SearchInput />
+    </>
+  )
+}
+
+export default App
