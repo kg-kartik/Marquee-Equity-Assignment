979a2867b98a49e25e728b21e7b40597dd05e7fd
kg-kartik <goel.kartik39@gmail.com>
2022-10-14 03:14:54 +0530

    minor styling and package changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
-import SearchInput from "./components/SearchInput";
+import SearchInput from "./components/Dropdown";
+import {Route,Switch} from "react-router-dom"
+import Details from "./pages/Details";
+import SearchBar from "./pages/SearchBar";
 
 const App = () => {
   return (
     <>
-		  <SearchInput />
+		  <Switch>
+        <Route path="/about">
+          <Details />
+        </Route>
+
+        <Route path="/">
+          <SearchBar />
+        </Route>
+
+        {/* <Route path="/search">
+          <Search />
+        </Route>       */}
+
+      </Switch>
     </>
   )
 }
